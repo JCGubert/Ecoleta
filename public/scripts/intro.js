65988e855b885c14f389371699578870d13ca65a
JCGubert <joaogubert85@gmail.com>
2020-06-09 22:35:51 -0300

    app

---
+++ public/scripts/intro.js
@@ -0,0 +1,60 @@
+// document.write("Hello")
+
+// variáveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+// document.write(myconst + myvar)
+
+//string
+//""
+//''
+//``
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+//boolean - true or false
+//const btrue = true
+//const bfalse = false
+//document.write(bfalse)
+
+// objetos possuem propriedades e funcionalidades
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//  correr(){
+//    document.write("executar uma grande lógica de correr")
+//  }
+//}
+//pessoa.correr()
+
+// Array - Vetores
+//const colecaoDeBolinhas = ["blue", "green", 1, {name: "My name"}]
+//document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+// Registrar função
+//function sayMyName(name) {
+//  document.write(name)
+//}
+// executar
+//sayMyName('João')
+//sayMyName('Ellen')
+//sayMyName('Vinícius')
+//sayMyName('Maria')
+
+//condicionais
+//const notaFinal = 7
+//if(notaFinal < 5) {
+//  document.write("Reprovado")
+//} else {
+//  document.write("Aprovado")
+//}
+
+// loop repetições
+//for (i=0; i<10; i++) {
+//  document.write(`<p>${i}</p>`)
+//}
